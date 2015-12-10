7f52757b1cd12c19095d0095bf1880f55d227bb0
bcowzer <bcowzer@hotmail.com>
2015-12-10 16:25:57 +0000

    solution to week 5 assignment xml parsing

---
+++ Extract_Data_week5.py
@@ -0,0 +1,25 @@
+import urllib
+import xml.etree.ElementTree as ET
+
+
+test_url = "http://python-data.dr-chuck.net/comments_42.xml"
+url = "http://python-data.dr-chuck.net/comments_183049.xml"
+
+# url = raw_input("Enter URL: ")
+
+xml = urllib.urlopen(url).read()
+
+tree = ET.fromstring(xml)
+
+counts = tree.findall('.//count')
+total = 0
+for count in counts:
+    total += int(count.text)
+
+print "Count: ", len(counts)
+print "Sum: ", total
+
+
+
+
+
